63ecef8eb6e7a3b8cb285f40b90d4ca4d0aa1746
dagiccross <dagiccross@yahoo.com>
2018-01-17 16:01:24 +0000

    add stable sourcefiles 20180116

---
+++ frontend/views/cart/index.php
@@ -0,0 +1,54 @@
+<div class="home-cat">
+  <div class="right-t1">
+    <div class="collect">
+      <p><input type="radio" name="cart[send]" value="collection">Collection</p>
+      <p>starts at 17:50</p>
+    </div>
+    <div class="deliver">
+      <p><input type="radio" name="cart[send]" value="deliver">Delivery</p>
+      <p>starts at 17:50</p>
+    </div>
+    <div class="clearfix"></div>
+  </div> 
+
+  <div class="order">
+    <div class="order-cart">
+      <span class="cart"><img src="<?php echo IMG_URL?>/cart.png"></span><span class="order-title">Your Order</span>
+      <div class="clearfix"></div>
+    </div>
+    
+    <div class="need">You need to spend $10 or<br/>more to order for  delivery</div>
+      <div class="order-menu">
+        <ul class="order-list">
+          <li>
+            <div class="cut-btn"><a href="#"><img src="<?php echo IMG_URL?>/cut-btn.png"></a></div>
+            <div class="order-name">Coca-Cola,0.33L</div>
+            <div class="pre-price">0.80</div>
+            <div class="clearfix"></div>
+          </li>
+          <li>
+            <div class="cut-btn"><a href="#"><img src="<?php echo IMG_URL?>/cut-btn.png"></a></div>
+            <div class="order-name">2 x Fried Chicken Wings & Chilli with Garlic</div>
+            <div class="pre-price"> 7.60</div>
+            <div class="clearfix"></div>
+          </li>
+        </ul>
+      </div>
+
+    <div class="total">
+      <p>Subtotal <span class="to-price">8.4047</span></p>
+      <p>Delivery fee <span class="to-price">1.00</span></p>
+      <p>Total<span class="to-price"> $9.40</span></p>
+    </div>
+
+    <div class="ormark">
+      <p class="note">Leave a note for the restaurant</p>
+      <textarea name="cart[note]" placeholder="e.g.if you have a food alergy or instrucitons for the driver"></textarea>
+      <p class="allergy">Do you have an allergy?</p>
+    </div>
+
+
+  </div>
+
+<div class="checkout"><input type="button" value="Go to checkout" name="checkout" class="checkout-btn"></div>
+</div>
