b82702166ce687218ea16b017ec3bc7bc1f525fa
dagiccross <dagiccross@yahoo.com>
2018-03-01 14:14:04 +0000

    adding both main-local.php & params-local.php back

---
+++ common/config/main-local.php
@@ -0,0 +1,29 @@
+<?php
+// company
+
+$user = '';
+$pw = '';
+$db = str_replace("o", "", $user);
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;port=3316;dbname='.$db.';unix_socket=/tmp/mysql5.sock',
+            //'dsn' => 'mysql:host=127.0.0.1;port=3316;dbname=db62110653;',
+            'username' => $user,
+            'password' => $pw,
+            'charset' => 'utf8',
+
+            'tablePrefix' => 'yii2_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
